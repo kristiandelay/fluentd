1e461a9efe296cd3be07c9fca95b681c184c8a51
TAGOMORI Satoshi <tagomoris@gmail.com>
2017-01-04 17:27:44 +0900

    make error message more descriptive

--- lib/fluent/supervisor.rb
+++ lib/fluent/supervisor.rb
@@ -428,7 +428,7 @@ module Fluent
       set_system_config
 
       if @workers < 1
-        raise Fluent::ConfigError, "invalid number of workers:#{@workers}"
+        raise Fluent::ConfigError, "invalid number of workers (must be > 0):#{@workers}"
       end
 
       if @root_dir
